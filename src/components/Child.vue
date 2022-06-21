1e85646fef9c8c6a7c13da1779b904480d547e4c
vijaykumar <vijay1111mane@gmail.com>
2022-06-21 15:02:50 +0530

    commit;

---
+++ src/components/Child.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>{{name}}</h2>
+  <h2>{{user.name}}</h2>
+  <button v-on:click="getData()">Call Function</button>
+</template>
+
+<script>
+export default {
+    name:"Child-Data",
+    props:{
+        name: String,
+        user: Object,
+        getData: Function
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1> Pass data to child component</h1>
+  <Child name="Vijay Mane" :user="user" :getData="getData" />
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+    name :"Parent-Data",
+    components:{
+        Child
+    },
+    data(){
+        return{
+            user:{
+                name: "vijay",
+                email:"vijay@gmail.com"
+            }
+        }
+    },
+    methods:{
+        getData(){
+            console.log("parent function called")
+            alert('hello');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
